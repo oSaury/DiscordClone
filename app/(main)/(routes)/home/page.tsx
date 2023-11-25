42801aa563b0457fce70760b0ef2e7df53de17c9
Ahmon <ahmonembaye@gmail.com>
2023-11-24 21:36:43 -0800

    Netflix

---
+++ app/(main)/(routes)/home/page.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { ActionTooltip } from "@/components/action-tooltip";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+
+const HomePage = async () => {
+    const router = useRouter();
+
+    const onClick = () => {
+        router.push(`/netflix/auth`);
+    }
+
+  return (
+    <ActionTooltip
+            side="right"
+            align="center"
+            label={"Netflix"}
+        >
+            <button
+                onClick={onClick}
+                className="group relative flex items-center"
+            >
+                <div className=
+                    "absolute left-0 bg-primary rounded-r-full transition-all w-[4px]"
+                />
+                <div className=
+                    "relative group flex mx-3 h-[100px] w-[100px] rounded-[50px] group-hover:rounded-[16px] transition-all overflow-hidden"
+                    
+                >
+                    <Image 
+                        fill
+                        src="https://utfs.io/f/395dee58-b1fa-4abb-917d-01d6afc0be50-p9h152.png"
+                        alt="Netflix"
+                    />
+
+                </div>
+            </button>
+
+
+        </ActionTooltip>
+    
+  )
+}
+ 
+export default HomePage;
--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -7,8 +7,9 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 import { currentProfile } from "@/lib/current-profile"
 import { db } from "@/lib/db";
 
-import { NavigationAction } from "./navigation-action";
-import { NavigationItem } from "./navigation-item";
+import { NavigationAction } from "@/components/navigation/navigation-action";
+import { NavigationItem } from "@/components/navigation/navigation-item";
+import { NavigationHome } from "./navigation-home";
 
 
 export const NavigationSideBar = async () => {
@@ -32,11 +33,9 @@ export const NavigationSideBar = async () => {
         <div
             className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] bg-[#E3E5E8] py-3"
         >
-            <NavigationAction
-            
-            />
+            <NavigationHome/>
             <Separator 
-                className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto"
+                    className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto"
             />
             <ScrollArea className="flex-1 w-full">
                 {servers.map((server) => (
@@ -49,6 +48,10 @@ export const NavigationSideBar = async () => {
                     </div>
                 ))}
             </ScrollArea>
+            <NavigationAction/>
+            <Separator 
+                    className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto"
+            />
             <div className="pb-3 mt-auto flex items-center flex-col gap-y-4">
                 <ModeToggle />
                 <UserButton 
@@ -59,6 +62,8 @@ export const NavigationSideBar = async () => {
                         }
                     }}
                 />
+                
+                
             </div>
         </div>
     )
