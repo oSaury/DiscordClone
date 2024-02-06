fd16a462aa244f5726658475256830070640ffb1
Ahmon Embaye <nottooshabby39@gmail.com>
2024-02-06 08:50:33 -0800

    [remove] async with useRouter

--- app/(main)/(routes)/home/page.tsx
+++ app/(main)/(routes)/home/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 
-const HomePage = async () => {
+const HomePage = () => {
     const router = useRouter();
 
     const onClick = () => {
